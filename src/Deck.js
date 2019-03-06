cc6635bd166d816c94881bd4c4bb2c10dc9024d9
Eric Essoyan <eric.essoyan@gmail.com>
2019-03-05 16:23:56 -0800

    animating single card

--- src/Deck.js
+++ src/Deck.js
@@ -5,19 +5,33 @@ class Deck extends Component {
     constructor(props) {
         super(props);
 
+        const position = new Animated.ValueXY();
         const panResponder = PanResponder.create({
-            onStartShouldSetPanResponder: () => {},
-            onPanResponderMove: () => {},
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y: gesture.dy })
+            
+            },
             onPanResponderRelease: () => {}
         });
 
-        this.state= { panResponder };
+        this.state= { panResponder, position };
         // this.panResponder = panResponder; << could also use this! will never call "setState"
     }
     
     
     renderCards() {
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        style={this.state.position.getLayout()}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
             return this.props.renderCard(item)
         })
     }
