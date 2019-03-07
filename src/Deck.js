705232c48f894d7c3c3e2f4730fd7fcfe13d52b0
Eric Essoyan <eric.essoyan@gmail.com>
2019-03-06 16:55:55 -0800

    defaultProps

--- src/Deck.js
+++ src/Deck.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {}
+    }
+    
     constructor(props) {
         super(props);
 
@@ -14,13 +21,34 @@ class Deck extends Component {
                 position.setValue({ x: gesture.dx, y: gesture.dy })
             
             },
-            onPanResponderRelease: () => {
-                this.resetPosition();
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
             }
         });
 
-        this.state= { panResponder, position };
+        this.state= { panResponder, position, index: 0 };
         // this.panResponder = panResponder; << could also use this! will never call "setState"
+    }
+
+    forceSwipe(direction) {
+        const x = direction === 'right' ? SCREEN_WIDTH * 1.5 : -SCREEN_WIDTH * 1.5;
+        Animated.timing(this.state.position, {
+            toValue: { x, y: 0},
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+    
+    onSwipeComplete(direction) {
+        const { onSwipeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.state.index];
+
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
     }
 
     resetPosition() {
